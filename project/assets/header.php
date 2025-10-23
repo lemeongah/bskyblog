abcac12b57d63cef15b4c14ed944f28f4ebb30a9
gillesah <helleugilles@gmail.com>
2025-10-23 11:36:31 +0200

    test

--- project/assets/header.php
+++ project/assets/header.php
@@ -11,7 +11,6 @@ if (!defined('ABSPATH')) {
 
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?>>
-
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
